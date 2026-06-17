25b58deaaa56cf0e8e80d82c6f75bc76c4f24754
Demaxxx <edmxliveira33@gmail.com>
2026-06-17 17:53:15 -0300

    Adicionando contador de instruções e testes de caso

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -2,12 +2,15 @@
 using namespace std;
 
 // FUNÇÃO INSERTION SORT
-int* insertion_sort(int lista[], int tamanho){
+int* insertion_sort(int lista[], int tamanho, int& cont){
+
+    //int cont = 0;
 
     for(int i = 1; i < tamanho; i++){
         int j = i - 1;
         int atual = lista[i];
         while(j >= 0 && lista[j] > atual){
+            cont++;
             lista[j + 1] = lista[j];
             j = j - 1;
         }
@@ -19,18 +22,51 @@ int* insertion_sort(int lista[], int tamanho){
 // FUNÇÃO PRINCIPAL
 int main(){
 
-    // vetor pra teste
-    int lista[] = {5, 3, 8, 1, 4};
-    int tamanho = 5;
-
-    int* lista_ordenada = insertion_sort(lista, tamanho);
+    // teste melhor caso (vetor ordenado)
+    int listaOrd[] = {1, 2, 3, 4, 5};
+    int tamanhoOrd = 5;
+    int contadorOrd = 0;
+    int* lista_ordenada = insertion_sort(listaOrd, tamanhoOrd, contadorOrd);
 
     // imprimindo
     cout << "Lista ordenada: ";
-    for (int i = 0; i < tamanho; i++) {
+    for (int i = 0; i < tamanhoOrd; i++) {
         cout << lista_ordenada[i] << " ";
     }
     cout << endl;
+    cout << "Total de instrucoes: " << contadorOrd << endl;
+
+    //===================================================================
+
+    // teste médio caso (vetor aleatorio)
+    int listaAle[] = {2, 1, 3, 5, 4};
+    int tamanhoAle = 5;
+    int contadorAle = 0;
+    int* lista_Aleatoria = insertion_sort(listaAle, tamanhoAle, contadorAle);
+
+    // imprimindo
+    cout << "Lista ordenada: ";
+    for (int i = 0; i < tamanhoAle; i++) {
+        cout << lista_Aleatoria[i] << " ";
+    }
+    cout << endl;
+    cout << "Total de instrucoes: " << contadorAle << endl;
+
+    //===================================================================
+
+    // teste pior caso (vetor decrescente)
+    int listaDec[] = {5, 4, 3, 2, 1};
+    int tamanhoDec = 5;
+    int contadorDec = 0;
+    int* lista_Decrescente = insertion_sort(listaDec, tamanhoDec, contadorDec);
+
+    // imprimindo
+    cout << "Lista ordenada: ";
+    for (int i = 0; i < tamanhoDec; i++) {
+        cout << lista_Decrescente[i] << " ";
+    }
+    cout << endl;
+    cout << "Total de instrucoes: " << contadorDec << endl;
 
     return 0;
 }
